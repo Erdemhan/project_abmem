c129ae91deb00932472badb315600a47e6b5a4bd
erdmhn <dev.erdemhan@gmail.com>
2023-12-06 14:56:20 +0300

    TEst code refactor

--- tries/django_model/test.py
+++ tries/django_model/test.py
@@ -1,17 +1,17 @@
 import db.models as models
-from db.models import enums
+from db.models.enums import *
 def main():
     simulation = models.simulation.Simulation(name='TEST',
-                                mode=enums.SimulationMode.STEPBYSTEP,
-                                state=enums.SimulationState.STARTED,
-                                periodType=enums.PeriodType.HOUR,
+                                mode=SimulationMode.STEPBYSTEP,
+                                state=SimulationState.STARTED,
+                                periodType=PeriodType.HOUR,
                                 periodNumber=10,
                                 currentPeriod=0,
                                 )
     simulation.save()
 
-    market = models.market.Market(strategy=enums.MarketStrategy.DAYAHEAD,
-                              state=enums.MarketState.BROADCASTING,
+    market = models.market.Market(strategy=MarketStrategy.DAYAHEAD,
+                              state=MarketState.BROADCASTING,
                               lowerBidBound=0,
                               upperBidBound=100,
                               simulation=simulation
@@ -21,7 +21,7 @@ def main():
     
 
 
-    resource = models.models.Resource(energyType=enums.EnergyType.FOSSIL,
+    resource = models.models.Resource(energyType=EnergyType.FOSSIL,
                                   name='Coal',
                                   fuelCost=20.5,
                                   emission=0.5)
