c571d1f59888d0419df3fd2e124155d0284b4911
erdmhn <dev.erdemhan@gmail.com>
2023-12-11 10:26:41 +0300

    Service Structure

--- test/services/market/market_service.py
+++ test/services/market/market_service.py
@@ -5,26 +5,36 @@ from decimal import Decimal
 
 def startPool(market: Market, lower: int, upper: int, periodNum: int) -> [Offer]:
     market.state = MarketState.WAITINGAGENTS
+    agents = market.agent_set.all()
+    # ParallelService.startPool()
     market.save()
     pass
 
 def marketAlgorithm(market: Market, offers: [Offer]) -> ([Offer],Decimal):
     market.state = MarketState.CALCULATING
+    # Market Algorithm / Calculate PTF
     market.save()
     pass
 
 def marketClearing(market: Market,offers: [Offer], ptf: Decimal) -> [Offer]:
     market.state = MarketState.MARKETCLEARING
+    # Market Clearing
     market.save()
     pass
 
 def saveToDb(market: Market,offers: [Offer], ptf: Decimal) -> None:
     market.state = MarketState.BROADCASTING
+    # for offer in offers save() / market.period.save()
     market.save()
     pass
 
-def createPeriod(market: Market,) -> Period:
+def createPeriod(market: Market) -> Period:
+    # PeriodFactory.Create()
     pass
 
 def readData() -> dict:
+    # ReaderService.readMarketData()
     pass
+
+
+
