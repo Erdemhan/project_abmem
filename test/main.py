1a5aef7a915f7d51d61e98f0aad8e1f46e489073
erdemhan <dev.erdemhan@gmail.com>
2024-03-18 16:30:21 +0300

    main.py path fix

--- test/main.py
+++ test/main.py
@@ -2,6 +2,7 @@ from django_model.db.models import *
 from django_model.db.models import enums
 from services.agent import agent_factory as AgentFactory
 from services.starter import starter_service
+sys.path.append("D:\lokal\Projeler\Tez\abm\abmem_project\test")
 
 def main():
     starter_service.start()
